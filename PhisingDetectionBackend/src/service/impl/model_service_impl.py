5805317c3d3915c2b31e57b7d9b763def18f61bb
Ghiffary Rifqialdi <grifqialdi@gmail.com>
2024-07-25 15:50:34 +0700

    fix peft bug

--- PhisingDetectionBackend/src/service/impl/model_service_impl.py
+++ PhisingDetectionBackend/src/service/impl/model_service_impl.py
@@ -2,9 +2,10 @@ from typing import Optional, Dict
 import os
 from utils.config import Config
 from loguru import logger
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM, AutoConfig
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM, AutoConfig, GenerationConfig
 from peft import PeftConfig, PeftModel
 from service.model_service import ModelService
+from munch import munchify
 
 class ModelServiceImpl(ModelService):
     _instance = None
@@ -36,39 +37,46 @@ class ModelServiceImpl(ModelService):
                          model_name: str) -> None:
         model_dir = "./" + Config.get().models.path + "/" +  model_name.replace('/', '_')
         model_name = model_dir if os.path.exists(model_dir) else model_name
+
+        # peft_config = PeftConfig.from_pretrained(model_name, token=os.getenv("HF_TOKEN"))
+        # base_model = AutoModelForSeq2SeqLM.from_pretrained(peft_config.base_model_name_or_path, 
+        #                                                            trust_remote_code=True,
+        #                                                            token=os.getenv("HF_TOKEN"))
+        # self.model = PeftModel.from_pretrained(base_model, model_name).to("cuda:0")
+        # self.tokenizer = AutoTokenizer.from_pretrained(peft_config.base_model_name_or_path, 
+        #                                                trust_remote_code=True,
+        #                                                token=os.getenv("HF_TOKEN"))
+        # self.is_peft = True
         
-        config = AutoConfig.from_pretrained(model_name,
-                                            trust_remote_code=True)
-        is_peft_model = True if hasattr(config, 'adapter_names') and config.adapter_names else False
-        
-        if is_peft_model:
+        config = munchify({"is_encoder_decoder": False})
+        peft_config = munchify({"base_model_name_or_path": model_name})
+        self.is_peft = False
+        try:
             logger.info(f"Loading PEFT model configuration")
             peft_config = PeftConfig.from_pretrained(model_name, token=os.getenv("HF_TOKEN"))
-            if config.is_encoder_decoder:
-                logger.info(f"Loading base Seq2Seq model: {model_name}")
-                base_model = AutoModelForSeq2SeqLM.from_pretrained(peft_config.base_model_name_or_path, 
-                                                                   trust_remote_code=True,
-                                                                   token=os.getenv("HF_TOKEN"))
-            else:
-                logger.info(f"Loading base Causal Language model: {model_name}")
-                base_model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path, 
-                                                                  trust_remote_code=True,
-                                                                  token=os.getenv("HF_TOKEN"))
-            logger.info("Applying PEFT adapter")
-            self.model = PeftModel.from_pretrained(base_model, model_name).to("cuda:0")
+            self.is_peft = True
+            config = AutoConfig.from_pretrained(peft_config.base_model_name_or_path,
+                                            trust_remote_code=True)
+        except:
+            config = AutoConfig.from_pretrained(model_name,
+                                                trust_remote_code=True)
+            
+        if config.is_encoder_decoder:
+            logger.info(f"Loading Seq2Seq model: {model_name}")
+            self.model = AutoModelForSeq2SeqLM.from_pretrained(peft_config.base_model_name_or_path if self.is_peft else model_name, 
+                                                                trust_remote_code=True,
+                                                                token=os.getenv("HF_TOKEN")).to("cuda:0")
         else:
-            if config.is_encoder_decoder:
-                logger.info(f"Loading Seq2Seq model: {model_name}")
-                self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, 
-                                                                   trust_remote_code=True,
-                                                                   token=os.getenv("HF_TOKEN")).to("cuda:0")
-            else:
-                logger.info(f"Loading Causal Language model: {model_name}")
-                self.model = AutoModelForCausalLM.from_pretrained(model_name, 
-                                                                  trust_remote_code=True,
-                                                                  token=os.getenv("HF_TOKEN")).to("cuda:0")
+            logger.info(f"Loading Causal Language model: {model_name}")
+            self.model = AutoModelForCausalLM.from_pretrained(peft_config.base_model_name_or_path if self.is_peft else model_name, 
+                                                                trust_remote_code=True,
+                                                                token=os.getenv("HF_TOKEN")).to("cuda:0")
         
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, 
+        if self.is_peft:
+            logger.info("Applying PEFT adapter")
+            self.model = PeftModel.from_pretrained(self.model, model_name).to("cuda:0")
+
+        self.tokenizer = AutoTokenizer.from_pretrained(peft_config.base_model_name_or_path if self.is_peft else model_name, 
                                                        trust_remote_code=True,
                                                        token=os.getenv("HF_TOKEN"))
         if not os.path.exists(model_dir):
@@ -99,15 +107,23 @@ class ModelServiceImpl(ModelService):
         {prompt}
         """
         inputs = self.tokenizer(prompt_template, return_tensors='pt').to(device="cuda:0")
-        
-        output_ids = self.model.generate(
-            inputs["input_ids"],
-            max_new_tokens=max_new_tokens,
-            temperature=temperature,
-            top_k=top_k,
-            top_p=top_p,
-            repetition_penalty=repetition_penalty
-        )
+        if self.is_peft:
+            input_ids = self.tokenizer(prompt_template, return_tensors="pt").to(device="cuda:0").input_ids
+            output_ids = self.model.generate(input_ids=inputs["input_ids"],
+                                             generation_config=GenerationConfig(max_new_tokens=max_new_tokens,
+                                                                                temperature=temperature,
+                                                                                top_k=top_k,
+                                                                                top_p=top_p,
+                                                                                repetition_penalty=repetition_penalty))
+        else:
+            output_ids = self.model.generate(
+                input_ids=inputs["input_ids"],
+                max_new_tokens=max_new_tokens,
+                temperature=temperature,
+                top_k=top_k,
+                top_p=top_p,
+                repetition_penalty=repetition_penalty
+            )
         completion = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
         result = {
             'completion': completion
